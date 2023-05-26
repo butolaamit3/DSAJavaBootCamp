8cda761743a6dfeba55229298337b86783105220
Amit <butolaamit3@gmail.com>
2023-05-26 09:50:19 +0530

    Pass by value and pass by value of a reference in functions

---
+++ src/com/methods/Swap.java
@@ -0,0 +1,32 @@
+package com.methods;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // swap numbers code
+//        int temp = a;
+//        a = b;
+//        b = temp;
+
+        swap(a, b);
+
+        System.out.println(a + " " + b);
+
+        String name = "Amit Butola";
+        changeName(name);
+        System.out.println(name);
+    }
+
+    static void changeName(String naam) {
+        naam = "Rahul Rana"; // creating a new object // does not change the name
+    }
+
+    static void swap(int num1, int num2) {
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+        // this change will only be valid in this function scope only.
+    }
+}
