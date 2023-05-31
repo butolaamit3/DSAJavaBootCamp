bf545ff06927a065827071017e8a811063606667
Amit <butolaamit3@gmail.com>
2023-05-31 07:57:24 +0530

    Finding max element in 2D array

---
+++ src/com/Searching/MaxIn2dArray.java
@@ -0,0 +1,21 @@
+package com.Searching;
+
+import java.util.Arrays;
+
+public class MaxIn2dArray {
+    public static void main(String[] args) {
+        int[][] arr = {{1,2,3},{4,5,6,},{7,54,8,9}};
+        System.out.println(max(arr));
+    }
+    public static int max(int[][]arr){
+        int max = Integer.MIN_VALUE;
+        for (int row = 0; row <arr.length ; row++) {
+            for (int col = 0; col <arr[row].length ; col++) {
+                if(arr[row][col]>max){
+                    max = arr[row][col];
+                }
+            }
+        }
+        return max;
+    }
+}
