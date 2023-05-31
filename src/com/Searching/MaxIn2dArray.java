ca21de6d84543a2920f5590ae664e2857e04b599
Amit <butolaamit3@gmail.com>
2023-05-31 08:39:05 +0530

    Enhance forloop method added

--- src/com/Searching/MaxIn2dArray.java
+++ src/com/Searching/MaxIn2dArray.java
@@ -7,12 +7,24 @@ public class MaxIn2dArray {
         int[][] arr = {{1,2,3},{4,5,6,},{7,54,8,9}};
         System.out.println(max(arr));
     }
+
     public static int max(int[][]arr){
         int max = Integer.MIN_VALUE;
         for (int row = 0; row <arr.length ; row++) {
             for (int col = 0; col <arr[row].length ; col++) {
                 if(arr[row][col]>max){
                     max = arr[row][col];
+                }
+            }
+        }
+        return max;
+    }
+    public static int max1(int[][]arr){
+        int max = Integer.MIN_VALUE;
+        for (int[] ints : arr) {
+            for (int element : ints) {
+                if (element > max) {
+                    max = element;
                 }
             }
         }
