5da0e77d0ce26c2dd1fcf528e3654f57bb075b04
Amit <butolaamit3@gmail.com>
2023-07-05 08:06:50 +0530

    Leetcode medium cyclic sort problem - 287. Find the Duplicate Number

---
+++ src/com/sorting/questions/FindDuplicate.java
@@ -0,0 +1,30 @@
+package com.sorting.questions;
+//Oues - 287. Find the Duplicate Number
+//https://leetcode.com/problems/find-the-duplicate-number/description/
+public class FindDuplicate {
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,2,2};
+        System.out.println(duplicate(arr));
+
+    }
+    static int duplicate(int[] nums){
+        int i = 0;
+        while(i<nums.length){
+            if(nums[i]!=i+1){
+                int correct = nums[i]-1;
+                if(nums[i]!=nums[correct]){
+                    int temp = nums[i];
+                    nums[i] = nums[correct];
+                    nums[correct] = temp;
+                }
+                else{
+                    return nums[i];
+                }
+            }
+            else{
+                i++;
+            }
+        }
+        return -1;
+    }
+}
